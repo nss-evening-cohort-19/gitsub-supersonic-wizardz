26746b832fc1ea9808171a7c9796fbf148cfc5a6
Shalane Proctor <shalane088@gmail.com>
2022-05-10 19:36:00 -0500

    Created form for repos page

--- scripts/repos.js
+++ scripts/repos.js
@@ -1,10 +1,37 @@
 import { skeletonDomString, renderToDom } from "./utlities.js";
 
 //define all your functions here
+const newRepoForm = () => {
+  const domString = `<h3>Create a new repository</h3>
+  <form>
+  <div class="form-floating">
+    <textarea
+      class="form-control"
+      placeholder="Repository Name"
+      id="floatingTextarea"
+    ></textarea>
+    <label for="floatingTextarea">Repository Name</label>
+  </div>
+  <h6>Great repository names are short and memorable. Need inspiration? How about <em>special-winner</em>?</h6>
+  <div class="form-floating">
+    <textarea
+      class="form-control"
+      placeholder="Description"
+      id="floatingTextarea2" style="height: 100px"></textarea>
+    <label for="floatingTextarea2">Description</label>
+  </div>
+  
+  <hr>
+  <button type="submit" class="btn btn-primary">Create Repository</button>
+  </form>
+  `;
+  renderToDom("#uploadContent", domString);
+};
 
 function startApp() {
-  renderToDom("#mainPage", skeletonDomString)
+  renderToDom("#mainPage", skeletonDomString);
+  newRepoForm();
   //put rest of start up here
 }
 
-startApp()
+startApp();
